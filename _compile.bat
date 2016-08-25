c730a93db8ef5e06ee7c27f4e61d114e50497ad6
Quentin Campos <quentin.campos@gmail.com>
2016-08-25 22:43:43 +0200

    Split the document into parts & update compile script

--- _compile.bat
+++ _compile.bat
@@ -1,1 +1,1 @@
-pandoc report.md -o report.pdf --template template.tex
+pandoc p0.intro.md p1.context.md p2.swh.md p3.chronology.md p4.archiver.md p5.objstorage.md p6.vault.md p7.conclusion.md -o report.pdf --template template/base.tex
