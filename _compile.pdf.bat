0c5d6863650b0a2b0463f1e27b9d87d3b0a05c45
qcampos <quentin.campos@gmail.com>
2016-08-26 00:08:34 +0200

    Update compile scripts
    Add the possibility to compile the markdown to Latex directly

---
+++ _compile.pdf.bat
@@ -0,0 +1,3 @@
+pandoc p0.intro.md p1.context.md p2.swh.md p3.chronology.md p4.archiver.md p5.objstorage.md p6.vault.md p7.conclusion.md -o report.pdf --template template/base.tex
+
+PAUSE
---
+++ _compile.tex.bat
@@ -0,0 +1,1 @@
+pandoc p0.intro.md p1.context.md p2.swh.md p3.chronology.md p4.archiver.md p5.objstorage.md p6.vault.md p7.conclusion.md -o report.tex --template template/base.tex
