1bfd103260e2c0084b66147110e853a361d90b56
Quentin Campos <quentin.campos@gmail.com>
2016-08-26 02:25:35 +0200

    Start creation of own template

--- _compile.pdf.bat
+++ _compile.pdf.bat
@@ -1,3 +1,6 @@
-pandoc p0.intro.md p1.context.md p2.swh.md p3.chronology.md p4.archiver.md p5.objstorage.md p6.vault.md p7.conclusion.md -o report.pdf --template template/base.tex
-
-PAUSE
+pandoc \
+pp.headers.md p0.intro.md p1.context.md p2.swh.md p3.chronology.md \
+p4.archiver.md p5.objstorage.md p6.vault.md p7.conclusion.md \
+-o report.pdf \
+--template template/custom.tex \
+-N
---
+++ pp.headers.md
@@ -0,0 +1,8 @@
+---
+  title: Software Heritage - Stage de fin d'étude
+  subtitle: Rapport de stage de fin d'étude réalisé au sein d'une équipe développant un projet libre
+  author: Quentin Campos
+  lang: french
+  documentclass: article
+  toc: True
+...
